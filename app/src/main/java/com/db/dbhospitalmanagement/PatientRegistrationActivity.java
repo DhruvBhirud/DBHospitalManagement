291f7d2eba194b1cc0b698e07ed7cb3636500e3d
DhruvBhirud <bhiruddhruv@gmail.com>
2022-10-23 23:22:34 +0530

    Added firebase storage and user profile

--- app/src/main/java/com/db/dbhospitalmanagement/PatientRegistrationActivity.java
+++ app/src/main/java/com/db/dbhospitalmanagement/PatientRegistrationActivity.java
@@ -1,19 +1,39 @@
 package com.db.dbhospitalmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -25,6 +45,9 @@ public class PatientRegistrationActivity extends AppCompatActivity {
     private Button regButton;
     private CircleImageView profileImage;
     private Uri resultUri;
+    private FirebaseAuth mAuth;
+    private DatabaseReference userDatabaseRef;
+    private ProgressDialog loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +70,9 @@ public class PatientRegistrationActivity extends AppCompatActivity {
         loginPassword = findViewById(R.id.loginPassword);
         regButton = findViewById(R.id.regButton);
         profileImage = findViewById(R.id.profileImage);
+
+        loader = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
 
         profileImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,7 +117,103 @@ public class PatientRegistrationActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    loader.setMessage("Registration is in progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()){
+                                String error = task.getException().toString();
+                                Toast.makeText(PatientRegistrationActivity.this,"Error Occurred: "+error,
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                String currentUserId = mAuth.getCurrentUser().getUid();
+                                userDatabaseRef = FirebaseDatabase.getInstance().getReference()
+                                        .child("users").child(currentUserId);
+                                HashMap userInfo = new HashMap();
+                                userInfo.put("name", fullName);
+                                userInfo.put("email", email);
+                                userInfo.put("idnumber", idNumber);
+                                userInfo.put("phonenumber", phoneNumber);
+                                userInfo.put("type", "patient");
 
+                                userDatabaseRef.updateChildren(userInfo).addOnCompleteListener(new OnCompleteListener() {
+                                    @Override
+                                    public void onComplete(@NonNull Task task) {
+                                        if (task.isSuccessful()){
+                                            Toast.makeText(PatientRegistrationActivity.this,"Details Set Successfully",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                        else{
+                                            Toast.makeText(PatientRegistrationActivity.this,task.getException().toString(),
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                        finish();
+                                        loader.dismiss();
+                                    }
+                                });
+
+                                if (resultUri != null){
+                                    final StorageReference filepath =
+                                            FirebaseStorage.getInstance().getReference().child("profile pictures").child(currentUserId);
+                                    Bitmap bitmap = null;
+                                    try {
+                                        bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(),resultUri);
+                                    }catch (IOException e){
+                                        e.printStackTrace();
+                                    }
+
+                                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                                    bitmap.compress(Bitmap.CompressFormat.JPEG,20,byteArrayOutputStream);
+                                    byte[] data = byteArrayOutputStream.toByteArray();
+                                    UploadTask uploadTask = filepath.putBytes(data);
+                                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            finish();
+                                            return;
+                                        }
+                                    });
+                                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                            if(taskSnapshot.getMetadata() != null){
+                                                Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
+                                                result.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                    @Override
+                                                    public void onSuccess(Uri uri) {
+                                                        String imageUrl = uri.toString();
+                                                        Map newImageMap = new HashMap();
+                                                        newImageMap.put("profilepictureurl",imageUrl);
+                                                        userDatabaseRef.updateChildren(newImageMap).addOnCompleteListener(new OnCompleteListener() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task task) {
+                                                                if (task.isSuccessful()){
+                                                                    Toast.makeText(PatientRegistrationActivity.this,"Registration was Successful",
+                                                                            Toast.LENGTH_SHORT).show();
+                                                                }
+                                                                else{
+                                                                    Toast.makeText(PatientRegistrationActivity.this,task.getException().toString(),
+                                                                            Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            }
+                                                        });
+                                                        finish();
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                                    Intent intent = new Intent(PatientRegistrationActivity.this,MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                    loader.dismiss();
+                                }
+                            }
+                        }
+                    });
                 }
             }
         });
@@ -100,7 +222,7 @@ public class PatientRegistrationActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode==1 && resultCode== Activity.RESULT_OK && data!=null){
+        if (requestCode==1 && resultCode== Activity.RESULT_OK && data!=null){
             resultUri=data.getData();
             profileImage.setImageURI(resultUri);
         }
